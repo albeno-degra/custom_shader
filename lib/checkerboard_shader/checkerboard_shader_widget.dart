4da0d087c39d7c72fbdc03dce68fff2208a083c2
ivankova.alexandra766 <ivankova.alexandra766@gmail.com>
2025-09-27 18:15:42 +0200

    colors fix

--- lib/checkerboard_shader/checkerboard_shader_widget.dart
+++ lib/checkerboard_shader/checkerboard_shader_widget.dart
@@ -3,17 +3,22 @@ import 'dart:ui' as ui;
 import 'package:custom_shader/checkerboard_shader/shader_cache.dart';
 import 'package:flutter/material.dart';
 
+const kAlpha = 255;
+
 class CheckerboardShaderWidget extends StatefulWidget {
   const CheckerboardShaderWidget({
     required this.child,
     required this.chessboardWidth,
     this.isTransparent = true,
-
+    this.color1 = const Color.fromARGB(kAlpha, 233, 233, 233),
+    this.color2 = const Color.fromARGB(kAlpha, 254, 254, 254),
     super.key,
   });
   final Widget child;
   final bool isTransparent;
   final double chessboardWidth;
+  final Color color1;
+  final Color color2;
 
   @override
   State<CheckerboardShaderWidget> createState() =>
@@ -51,12 +56,12 @@ class _CheckerboardShaderWidgetState extends State<CheckerboardShaderWidget> {
             final shader = snapshot.data!.fragmentShader()
               ..setFloat(0, widget.chessboardWidth)
               ..setFloat(1, widget.chessboardWidth)
-              ..setFloat(2, 233 / 255)
-              ..setFloat(3, 233 / 255)
-              ..setFloat(4, 233 / 255)
-              ..setFloat(5, 254 / 255)
-              ..setFloat(6, 254 / 255)
-              ..setFloat(7, 254 / 255);
+              ..setFloat(2, widget.color1.r)
+              ..setFloat(3, widget.color1.g)
+              ..setFloat(4, widget.color1.b)
+              ..setFloat(5, widget.color2.r)
+              ..setFloat(6, widget.color2.g)
+              ..setFloat(7, widget.color2.b);
             return shader;
           },
           child: widget.child,
